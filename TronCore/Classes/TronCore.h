b17a53ea09387ea5c911522f5076d63ddbbee375
elon xu <elon.xu@tron.network>
2024-01-27 10:48:48 +0800

    fix TronCore

---
+++ TronCore/Classes/TronCore.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrustCore.
+FOUNDATION_EXPORT double TrustCoreVersionNumber;
+
+//! Project version string for TrustCore.
+FOUNDATION_EXPORT const unsigned char TrustCoreVersionString[];
+
+#import <TronCore/EthereumCrypto.h>
